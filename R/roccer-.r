cb709a5146c49d32a0437c314ee4d615c40cb66c
hadley <h.wickham@gmail.com>
2012-08-25 09:37:42 -0500

    Move all roccer code into one file

--- R/roccer-.r
+++ R/roccer-.r
@@ -34,3 +34,30 @@ find_roccers <- function(env = asNamespace("roxygen3")) {
   rocs
 }
 
+
+#' @import igraph
+roccer_graph <- function(roccers, prereqs) {
+  nodes <- names(roccers)
+
+  n <- vapply(prereqs, length, integer(1))
+  edge_df <- cbind(unlist(prereqs), rep(names(prereqs), n))
+  node_df <- data.frame(name = nodes)
+
+  graph <- graph.data.frame(edge_df, vertices = node_df)
+  V(graph)$label <- V(graph)$name
+
+  graph
+}
+
+sort_roccers <- function(roccers, prereqs = NULL) {
+  if (is.null(prereqs)) return(roccers)
+  
+  g <- roccer_graph(roccers, base_prereqs)
+  order <- V(g)$name[topological.sort(g)]
+  roccers[order]
+}
+
+base_roccers <- function() {
+  base <- find_roccers(asNamespace("roxygen3"))
+  sort_roccers(base, base_prereqs)
+}
