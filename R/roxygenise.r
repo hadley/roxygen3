e87928b5fbd50d36d4683367ce706010322e8f6d
hadley <h.wickham@gmail.com>
2012-08-25 09:29:57 -0500

    Update roxygenise function

--- R/roxygenise.r
+++ R/roxygenise.r
@@ -1,14 +1,22 @@
 #' @param roccer a list of roccers that you want to use.  By default these
-#'   will just be the roccers provided by 
-roxygenise <- function(path, env = NULL, roccers = base_roccers()) {
-
-  rocblocks <- parse_directory(path, env)
+#'   will just be the roccers provided by roxygen3.
+#' @param path path to package
+#' @auto_imports
+roxygenise <- function(path, roccers = base_roccers()) {
+  pkg <- as.package(path)
   
-  # Roccers need to be run in a specific order and later roccers need to be
-  # able to see results of earlier roccers.
-  for (roccer in roccers) {
-    rocblocks <- parse_rocblocks(roccer$parser, rocblocks)
-  }
+  load_all(pkg)
+  in_dir(pkg$path, {
+    rocblocks <- parse_directory("R", ns_env(pkg))
 
-  # Rocouts can be run in any order and are completely independent.
+    # Roccers need to be run in a specific order and later roccers need to be
+    # able to see results of earlier roccers.
+    for (roccer in roccers) {
+      rocblocks <- parse_rocblocks(roccer$parser, rocblocks)
+    }
+
+    # Rocouts can be run in any order and are completely independent.
+    write_out(roccers, rocblocks, ".")
+    
+  })
 }
