d54c53bd9b5462ce12c36fa72d04f0604e565bd4
hadley <h.wickham@gmail.com>
2012-08-27 15:37:42 -0500

    Tests for S4 usage

--- R/doctype-s4-method.r
+++ R/doctype-s4-method.r
@@ -1,9 +1,19 @@
 doctype.MethodDefinition <- function(obj) "method"
 
 usage.MethodDefinition <- function(obj, name) {
-  signature <- str_c(as.character(obj@defined), collapse = ",")
+  args <- usage_args(formals(obj))
+  new_usage(
+    args = usage_args(formals(obj@.Data)),
+    generic = obj@generic,
+    signature = obj@defined,
+    subclass = "usage_s4method")
+}
+
+format.usage_s4method <- function(x, ...) {
+  signature <- str_c(as.character(x$signature), collapse = ",")
   
-  str_c("\\S4method{", obj@generic, "}{", signature, "}")
+  str_c("\\S4method{", x$generic, "}{", signature, "}(", 
+    args_string(x$args), ")")
 }
 
 default_export.MethodDefinition <- function(obj, name) {
