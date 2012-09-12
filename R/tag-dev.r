8023ca82d4a3f858e3ca2cf68c847cd6c9b18709
hadley <h.wickham@gmail.com>
2012-09-12 15:19:05 -0500

    Improve @dev description text

--- R/tag-dev.r
+++ R/tag-dev.r
@@ -1,13 +1,13 @@
 setClass("TagDev", contains = "Tag")
 
 setMethod("procBlock", "TagDev", function(tag, block) {
-  desc <- "This function is useful only for developers"
-  
+  desc <- "This function is aimed primarily at developers extending this package. You shouldn't need to use it for everyday operation of the package."
+
   title <- block@tags$title
   if (!is.null(title)) {
     title@text <- str_c("[DEV] ", title@text)
   }
-  
+
   modify_tags(block,
     title = title,
     description = prefix(desc))
