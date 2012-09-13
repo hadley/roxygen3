7ecf263be02a6e2bca0f809c948d64bd609c5620
hadley <h.wickham@gmail.com>
2012-09-13 06:53:44 -0500

    Fix @dev bug

--- R/tag-dev.r
+++ R/tag-dev.r
@@ -3,7 +3,8 @@ setClass("DevTag", contains = "Tag")
 setMethod("process", "DevTag", function(input, block) {
   desc <- "This function is aimed primarily at developers extending this package. You shouldn't need to use it for everyday operation of the package."
 
-  title@text <- str_c("[DEV] ", tag_value(block, "title"))
+  title <- tag(block, "title")
+  title@text <- str_c("[DEV] ", value(title))
   tag(block, "title") <- title
 
   tag(block, "description") <- prefix(desc)
