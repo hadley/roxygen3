2df575af15f5408f93dd3959389f89d1a02c84ff
hadley <h.wickham@gmail.com>
2012-09-21 08:45:06 -0500

    Simplify @param value<-

--- R/tag-param.r
+++ R/tag-param.r
@@ -5,14 +5,7 @@ setMethod("value", "ParamTag", function(tag) tag@arguments)
 
 setMethod("value<-", "ParamTag", function(tag, value) {
   pieces <- str_split_fixed(value, "[[:space:]]+", 2)
-
-  name <- pieces[, 1]
-  desc <- str_trim(pieces[, 2])
-
-  # if (any(name == "")) {
-  #   stop(key, ' requires a name and description')
-  # }
-  tag@arguments <- setNames(desc, name)
+  tag@arguments <- setNames(pieces[, 2], pieces[, 1])
   tag
 })
 
