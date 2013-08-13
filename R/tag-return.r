2dc14d87a423176d87ba1cecb38e5b46a3b87a23
hadley <h.wickham@gmail.com>
2013-08-13 15:13:19 -0500

    Add note to @return

--- R/tag-return.r
+++ R/tag-return.r
@@ -1,7 +1,8 @@
 #' @@return: describe the output of the function
 #'
-#' @tagUsage @return What the function returns.  This can be a setnence of two
-#'   or several paragraphs.
+#' @tagUsage @return What the function returns.  This can be a sentence or two
+#'   or several paragraphs. If the output is a list of several components,
+#'   you can use \code{\item{comp_i}{Description of comp_i.}} to document them.
 setClass("ReturnTag", contains = "Tag")
 
 setMethod("writeRd", "ReturnTag", function(object) {
